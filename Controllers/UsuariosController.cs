a71e4e821636aa104fe7aa35cf1f83a79c9dd7ea
felipeferrarir <felipeferrari2007@gmail.com>
2024-12-06 14:11:35 -0300

    Subindo última parte do trabalho

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -3,6 +3,9 @@ using Exo.WebApi.Repositories;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace Exo.WebApi.Controllers
 {
@@ -28,11 +31,40 @@ namespace Exo.WebApi.Controllers
 
 
         // post -> /api/usuarios
-        [HttpPost]
+        /* [HttpPost]
         public IActionResult Cadastrar(Usuario usuario)
         {
            _usuarioRepository.Cadastrar(usuario);
            return StatusCode(201);
+        } */
+
+        public IActionResult Post(Usuario usuario)
+        {
+            Usuario usuarioBuscado = _usuarioRepository.Login(usuario.Email, usuario.Senha);
+            if (usuarioBuscado == null)
+            {
+                return NotFound("Email ou senha inválidos");
+            }
+
+            var claims = new[]
+            {
+        new Claim(JwtRegisteredClaimNames.Email, usuarioBuscado.Email),
+        new Claim(JwtRegisteredClaimNames.Jti, usuarioBuscado.Id.ToString()),
+    };
+
+    var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("exoapi-chave-autenticacao"));
+    var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+    var token = new JwtSecurityToken(
+        issuer: "exoapi.webapi",
+        audience: "exoapi.webapi",
+        claims: claims,
+        expires: DateTime.Now.AddMinutes(30),
+        signingCredentials: creds
+    );
+
+    return Ok(
+        new { token = new JwtSecurityTokenHandler().WriteToken(token) }
+    );
         }
 
         // get -> /api/usuarios/{id}
@@ -48,7 +80,8 @@ namespace Exo.WebApi.Controllers
         }
 
         // put -> /api/usuarios/{id}
-        // Atualiza.
+        
+        [Authorize]
         [HttpPut("{id}")]
         public IActionResult Atualizar(int id, Usuario usuario)
         {
@@ -57,6 +90,7 @@ namespace Exo.WebApi.Controllers
         }
 
         // delete -> /api/usuarios/{id}
+        [Authorize]
         [HttpDelete("{id}")]
         public IActionResult Deletar(int id)
         {
